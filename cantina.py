9fffe1ac685e1da6cae8564efb618b50817ba6ed
Jonathan <jonatan.borges2012@hotmail.com>
2025-02-16 11:51:32 -0300

    Adicionando as funcoes para escolhaCantina e escolhaOpcao

--- cantina.py
+++ cantina.py
@@ -1,8 +1,46 @@
-print("Seja Bem-Vindo!")
-print("\n")
-print("Escolha sua Cantina:")
-print("\n")
+def escolhaCantina(cantina):
+    match cantina:
+        case 1:
+            print("Você escolheu a Cantina UMP")
+        case 2:
+            print("Você escolheu a Cantina UPH")
+        case 3:
+            print("Você escolheu a Cantina UPA")
+        case 4:
+            print("Você escolheu a Cantina SAF")
+        case _:
+            print("Opção inválida. Escolha entre 1 e 4.")
+            return False
+
+    print("\nEscolha uma opção:")
+    print("1 - Fazer pedido")
+    print("2 - Cardápio do dia")
+    print("3 - Voltar")
+    print("4 - Sair")
+    return True
+
+def escolhaOpcao(opcao):
+    
+    match opcao:
+        case 1:
+            print("\nFazer pedido")
+        case 2:
+            print("\nCardápio do dia")
+        case 3:
+            print("\nVoltando ao menu...")
+        case 4:
+            print("\nSaindo... Até logo!")
+        case _:
+            print("\n Opção inválida. Escolha entre 1 e 4.")
+
+print("Seja Bem-Vindo! \n")
 print("1 - UMP - União de Mocidade Presbiteriana")
 print("2 - UPH - União Presbiteriana de Homens")
-print("3 - UPA - União Presbiteriana de Adolecentes")
-print("4 - SAF - Sociedade Auxiliadora Feminina")
+print("3 - UPA - União Presbiteriana de Adolescentes")
+print("4 - SAF - Sociedade Auxiliadora Feminina\n")
+
+cantina = int(input("Escolha uma cantina: "))
+
+if escolhaCantina(cantina):
+    opcao = int(input("\nEscolha uma opção: "))
+    escolhaOpcao(opcao)
